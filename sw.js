da507bc289a15bfb3f9960117f0a92fea17ecd5b
tresor676 <habimanatresor676@gmail.com>
2025-12-25 17:33:40 +0200

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'ludo-pwa-v1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/ludo.js',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
